b0fd45a7516ef067b91a6373bf1043a4fcb1286a
Frank Collins <fwc3rd@gmail.com>
2023-04-29 21:53:52 -0400

    [9:53pm]

---
+++ dumpsterdive.c
@@ -0,0 +1,9 @@
+ Preliminary input that allows a user to look into the trash for anything they might want before removing The items.
+ As I type this it woudl be interesting to tag files. Possibly with (i.e.)
+ mynotes.txt
+ 
+ mynotes_y.txt 
+ mark files with a regex-friendly _y to parse for characters after underscore. If such a file exists. You can't automatically remove it by clicking clear r
+ refuse bin.
+   
+  The bin will refuse to toss out those files.
