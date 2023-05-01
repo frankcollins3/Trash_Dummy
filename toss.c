d87988626f956b667793dc1eb4ca7d2a85921545
Frank Collins <fwc3rd@gmail.com>
2023-05-01 10:22:38 -0400

    create file within the vs code scope [10:28am]

--- toss.c
+++ toss.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <termios.h>
-#include <sys/ioctl.h>
 
-int main () {
-
-    FILE *trash;
+int main() {
+    FILE *trash, *newfile;
     char ch;
-    
+
     printf("test print:\t  found some pie. \n");
 
     trash = fopen("./test.txt", "r");
-
     if (trash == NULL) {
         printf("Failed to open file\n");
         return 1;
+    } else {
+        printf("trash is not equal to null. pretty cool\n");
+
+        while ((ch = fgetc(trash)) != EOF) {
+            printf("%c", ch);
+        }
+
+        fclose(trash);
     }
 
-    while ((ch = fgetc(trash)) != EOF) {
-        printf("%c", ch);
-    }
-
-    fclose(trash);
+    newfile = fopen("test2.txt", "w");
+    if (newfile == NULL) {
+        printf("Failed to open file for writing\n");
+        return 1;
+    } 
 
     return 0;
 }
