2434efa90ebc440504a4b10f51c82a32cf71121f
Frank Collins <fwc3rd@gmail.com>
2023-05-01 10:29:53 -0400

    proudly digging into the trash 🗑 [10:29am]
    the testtrash.txt file is on the desktop.
    
    
    lol last commit wasn't at 10:28 it was at 10:22 that would be insane speed.

--- toss.c
+++ toss.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    FILE *trash, *newfile;
+    FILE *trash, *newfile, *digtrash;
     char ch;
 
     printf("test print:\t  found some pie. \n");
@@ -11,20 +11,29 @@ int main() {
         printf("Failed to open file\n");
         return 1;
     } else {
-        printf("trash is not equal to null. pretty cool\n");
+        // printf("trash is not equal to null. pretty cool\n");
 
-        while ((ch = fgetc(trash)) != EOF) {
-            printf("%c", ch);
-        }
+        // while ((ch = fgetc(trash)) != EOF) {
+        //     printf("%c", ch);
+        // }
 
-        fclose(trash);
+        // fclose(trash);
     }
 
-    newfile = fopen("test2.txt", "w");
-    if (newfile == NULL) {
-        printf("Failed to open file for writing\n");
-        return 1;
-    } 
+    // newfile = fopen("test2.txt", "w");
+    // if (newfile == NULL) {
+    //     printf("Failed to open file for writing\n");
+    //     return 1;
+    // } 
+
+    digtrash = fopen("/Users/medium/Desktop/testtrash.txt", "r");
+    if (digtrash == NULL) {
+        printf("wow what garbage");
+    } else {
+        while ((ch = fgetc(digtrash)) != EOF) {
+            printf("%c", ch);
+        }
+    }
 
     return 0;
 }
