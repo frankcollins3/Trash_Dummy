5eb28bd54751e8019a1ba6448b9c2f6abb0c8dbb
Frank Collins <fwc3rd@gmail.com>
2023-05-01 09:52:39 -0400

    dont delete v.s. send files elsewhere [9:52pm]

--- dumpsterdive.c
+++ dumpsterdive.c
@@ -6,4 +6,7 @@
  mark files with a regex-friendly _y to parse for characters after underscore. If such a file exists. You can't automatically remove it by clicking clear r
  refuse bin.
    
+  
   The bin will refuse to toss out those files.
+  
+📖 considering the use this file to port over the items that wont be deleted from the trash.
